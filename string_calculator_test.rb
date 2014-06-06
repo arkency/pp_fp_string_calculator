e45b4529e473e06bb6afcf6e582792352acb422e
Filip Pacanowski <fpacanowski@gmail.com>
2014-06-06 15:38:20 +0200

    Test for adding single number

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,10 +2,15 @@ require 'test/unit'
 require './string_calculator'
 
 class StringCalculatorTest < Test::Unit::TestCase
-
-  def test_add_on_empty_string
-    calculator = StringCalculator.new
-    assert_equal 0, calculator.add("")
+  def setup
+    @calculator = StringCalculator.new
   end
 
+  def test_add_on_empty_string
+    assert_equal 0, @calculator.add("")
+  end
+
+  def test_add_single_number
+    assert_equal 7, @calculator.add("7")
+  end
 end
