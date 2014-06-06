caae0c9b9982c1a5c0facd7b0cdbdc34a2461eb3
Filip Pacanowski <fpacanowski@gmail.com>
2014-06-06 22:16:11 +0200

    Make test pass

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,17 @@
 class StringCalculator
   def add(numbers)
-    numbers.split(/[,\n]/).map(&:to_i).inject(0, :+)
+    delimeter, numbers = extract_delimeter(numbers)
+    delimeter_regexp = Regexp.new("[#{delimeter}\\n]")
+    numbers.split(delimeter_regexp).map(&:to_i).inject(0, :+)
+  end
+
+  private
+  def extract_delimeter(str)
+    match = str.match Regexp.new('^//(.*)\n')
+    if match.nil?
+      [",", str]
+    else
+      [match[1], str[match[0].length..-1]]
+    end
   end
 end
